f66488dcfa0d25719488a634232e9a6b624169ba
profVallone <profvallone@gmail.com>
2022-03-24 13:35:34 +0100

    aggiunta data di assunzione

--- Azienda/Program.cs
+++ Azienda/Program.cs
@@ -8,36 +8,31 @@ namespace GestioneAzienda
         static void Main(string[] args)
         {
             Azienda az = new Azienda();
-            Dipendente d1 = new Operaio("Mario", "Rossi", 38);
-            Dipendente d2 = new CapoReparto("Mario", "Bianchi", 25);
-            Dipendente d3 = new Dirigente("Franco", "Verdi", 54);
-            Dipendente d4 = new Operaio("Francesco", "Salvi", 20);
-            Dipendente d5 = new Operaio("Mattia", "Verdi", 34);
-            Dipendente d6 = new CapoReparto("Maria", "Franca", 34);
-            Dipendente d7 = new Dirigente("Giuseppe", "Bianchini", 64);
-            az.add(d1);
-            az.add(d2);
-            az.add(d3);
-            az.add(d4);
-            az.add(d5);
-            az.add(d6);
-            az.add(d7);
+            az.add(new Operaio("Mario", "Rossi", 38, "02-03-2001"));
+            az.add(new CapoReparto("Mario", "Bianchi", 25, "02-03-2015"));
+            az.add(new Dirigente("Franco", "Verdi", 54, "02-03-1992"));
+            az.add(new Operaio("Francesco", "Salvi", 20, "02-03-2010"));
+            az.add(new Operaio("Mattia", "Verdi", 34, "02-03-1989"));
+            az.add(new CapoReparto("Maria", "Franca", 34, "02-03-1989"));
+            az.add(new Dirigente("Giuseppe", "Bianchini", 64, "02-03-2005"));
             
             Console.WriteLine("-------Azienda-------------------------");
-            //inizializza in maniera Random i giorni lavorativi di ogni dipendente da 1 a 100
+            //inizializza in maniera Random i giorni lavorativi di ogni dipendente
+            //da 1 a 100
             //attraverso il metodo
             //addGiorni(int g); della classe Dipendente
             az.giorniRandom();
             Console.WriteLine(az);
             
             Console.WriteLine("-------Richiesta ferie-------------------------");
-            //per ogni dipendente vengono richiesti giorni di ferie random tra 1 e 100
+            //per ogni dipendente vengono richiesti giorni di ferie random
+            //tra 1 e 50
             //attraverso il metodo
             //prendiFerie(int g); della classe Dipendente
             az.prendiFerieRandom();
            
             Console.WriteLine("-------Cessione ferie-------------------------");
-            //per ogni dipendente vengono ceduti giorni di ferie random tra 1 e 100
+            //per ogni dipendente vengono ceduti giorni di ferie random tra 1 e 50
             //ad un altro dipendente scelto a caso
             //attraverso il metodo
             //cediFerie(Dipendente d, int g); della classe Dipendente
